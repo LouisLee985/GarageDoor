0455a5cc82c79e8e44c46802b06c6b69bdb640d2
Louis <88962972+LouisLee985@users.noreply.github.com>
2024-03-30 16:53:55 +0800

    Create extract_zip.bat

---
+++ make_bom/extract_zip.bat
@@ -0,0 +1,4 @@
+powershell -Command "Expand-Archive -Path 'python.zip' -DestinationPath 'InteractiveHtmlBom' -Force"
+del python.zip
+
+rem Expand-Archive -Force -Path python.zip -DestinationPath InteractiveHtmlBom
