a70ff7d67b8dd1ba8f6d8d9239ee7344afa0dd87
Louis <88962972+LouisLee985@users.noreply.github.com>
2024-03-30 17:05:36 +0800

    Update extract_zip.bat

--- make_bom/extract_zip.bat
+++ make_bom/extract_zip.bat
@@ -1,4 +1,5 @@
 powershell -Command "Expand-Archive -Path 'python.zip' -DestinationPath 'InteractiveHtmlBom' -Force"
 del python.zip
+del extract_zip.bat
 
 rem Expand-Archive -Force -Path python.zip -DestinationPath InteractiveHtmlBom
