985ce02f050558672ce0b8f25ea9f3ff333cedf8
Louis <88962972+LouisLee985@users.noreply.github.com>
2025-12-03 21:04:37 +0800

    Del

--- make_bom/extract_zip.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-powershell -Command "Expand-Archive -Path 'python.zip' -DestinationPath 'InteractiveHtmlBom' -Force"
-robocopy %~dp0 "%~dp0InteractiveHtmlBom" config.ini
-::del python.zip
-::del extract_zip.bat
-del /q "%~dp0"\*
-exit
--- make_bom/readme.md
+++
@@ -1,3 +0,0 @@
-* InteractiveHtmlBom is from https://github.com/openscopeproject/InteractiveHtmlBom/tree/master/InteractiveHtmlBom
-
-* python.zip is from https://github.com/yanranxiaoxi/InteractiveHtmlBom
